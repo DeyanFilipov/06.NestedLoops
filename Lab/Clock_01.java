e8f4468d7e5a28c851aef841baf869a37e95168b
DeyanFilipov <didkofilipov.sv@gmail.com>
2022-10-15 10:42:09 +0300

    Commit

---
+++ Lab/Clock_01.java
@@ -0,0 +1,14 @@
+package NestedLoops.Lab;
+
+public class Clock_01 {
+    public static void main(String[] args) {
+
+        for (int hour = 0; hour <= 23; hour++) {
+            for (int min = 0; min <= 59 ; min++) {
+                System.out.printf("%d:%d%n", hour, min);
+
+            }
+
+        }
+    }
+}
